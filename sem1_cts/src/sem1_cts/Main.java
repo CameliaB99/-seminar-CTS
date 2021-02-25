49c1c8035b3ad23536de23d5bf3be2987b9c2f68
CameliaB99 <72302424+CameliaB99@users.noreply.github.com>
2021-02-25 11:15:18 +0200

    finalul seminarului 1

--- sem1_cts/src/sem1_cts/Main.java
+++ sem1_cts/src/sem1_cts/Main.java
@@ -1,10 +1,32 @@
 package sem1_cts;
 
+import clase.Girafa;
+import clase.Zebra;
+import clase.Zoo;
+import clase.Zookeper;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//syso-ca sa apara singur
 		System.out.println("hello");
+		Zoo zoo = new Zoo();
+		Zookeper keeper = new Zookeper();
+		
+		zoo.setKeeper(keeper);
+		keeper.setName("Marcel");
+		Zebra zebra1= new Zebra("z1");
+		Zebra zebra2 = new Zebra("z2");
+		zoo.add(zebra1);
+		zoo.add(zebra2);
+		zoo.feedAll();
+		Girafa girafa1 =  new Girafa("Lily");
+		Girafa girafa2 =  new Girafa("Maia");
+		zoo.add(girafa1);
+		zoo.add(girafa2);
+		zoo.feedAll();
+		
 	}
 
 }
