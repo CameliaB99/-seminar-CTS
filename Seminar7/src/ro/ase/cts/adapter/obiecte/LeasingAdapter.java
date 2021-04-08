2a8e2018575e1044d48dffd22a743812e9e4cd71
CameliaB99 <72302424+CameliaB99@users.noreply.github.com>
2021-04-08 10:13:11 +0300

    Adapter de obiecte si de clase

---
+++ Seminar7/src/ro/ase/cts/adapter/obiecte/LeasingAdapter.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.adapter.obiecte;
+
+public class LeasingAdapter implements Creditable {
+	private Leasing leasing;
+	@Override
+	public void oferaCredit() {
+		this.leasing.oferaLeasing();
+		
+	}
+	public LeasingAdapter(Leasing leasing) {
+		super();
+		this.leasing = leasing;
+	}
+	
+
+}
